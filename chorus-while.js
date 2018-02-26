b4beb2fe7744f139098a39edee02be89b91799fd
Michael Stranges <strangesm@gmail.com>
2018-02-26 21:02:46 +0000

    loop excercises

---
+++ chorus-while.js
@@ -0,0 +1,17 @@
+var chorus = "Let's dance!";
+//var repeat = 0;
+/*while(repeat<10){
+  if(repeat === 5){
+    console.log("change key");
+  }
+  console.log(chorus);
+  repeat++;
+}
+
+console.log("Until the sun comes up!");
+*/
+
+for (var repeat = 0; repeat < 10; repeat++){
+  console.log(chorus);
+}
+console.log("Until the sun comes up!");
