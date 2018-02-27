2797cf5c1a2e8f52bf68acd78c515a27721d54a9
Michael Stranges <strangesm@gmail.com>
2018-02-27 03:07:18 +0000

    index activity 1

---
+++ lastIndexOf.js
@@ -0,0 +1,25 @@
+function lastIndexOf(array, value)
+{
+  var i;
+  for (i = 0; i <= array.length; i++)
+    {
+    if(array[i] == value)
+      {
+      var theindex = i;
+    }
+  }
+  if(theindex === undefined)
+    {
+    return -1;
+  }
+  else
+    {
+    return theindex;
+  }
+}
+
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 1), "=?", 3);
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 2), "=?", 4);
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 3), "=?", -1);
+console.log(lastIndexOf([ 5, 5, 5 ], 5), "=?", 2);
+console.log(lastIndexOf([], 3), "=?", -1);
