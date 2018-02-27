6e8a4422a1b520f69aa622862a3f1d8095ae8911
Michael Stranges <strangesm@gmail.com>
2018-02-27 04:12:58 +0000

    concat fun

--- concat-arrays.js
+++ concat-arrays.js
@@ -1,6 +1,12 @@
-function concat(array1, array2){
+/*function concat(array1, array2){
   var array3 = array1.concat(array2);
   return array3;
+}
+*/
+function concat(arrayOne, arrayTwo) {
+  for (var i = 0; i < arrayTwo.length; i++) {
+    arrayOne.push(arrayTwo[i]);
+  } return arrayOne;
 }
 
 console.log(concat([ 1, 2, 3 ], [ 4, 5, 6 ]), "=?", [ 1, 2, 3, 4, 5, 6 ]);
